be2d102d2bb67199035737d1624e8996e1bb6e3a
caojin16 <310869927@qq.com>
2024-10-09 10:52:08 +0800

    sadasd

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite and TypeScript logos to learn more实打实大时代
     </p>
   </div>
 `
